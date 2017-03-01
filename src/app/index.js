f2a3abe2648ffef7ff458084a1dc66de84d44f52
Zaneta <jazdzyk.zaneta@gmail.com>
2017-03-01 02:03:25 +0100

    react, babel, webpack - added; initial webpack config

---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nette on 01.03.17.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/**
+ * Created by nette on 01.03.17.
+ */
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
