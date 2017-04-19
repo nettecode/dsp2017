dcbbcc8943a34da70739149f830b6e937aac57a0
Zaneta <jazdzyk.zaneta@gmail.com>
2017-04-19 13:34:04 +0200

    react routing added, calendar & settings views added

---
+++ src/components/CalendarView/CalendarView.js
@@ -0,0 +1,17 @@
+/**
+ * Created by nette on 19.04.17.
+ */
+import React from 'react';
+
+const CalendarView = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h2>Kalendarz</h2>
+                <p>... tu będzie :P</p>
+            </div>
+        );
+    }
+});
+
+export default CalendarView;
